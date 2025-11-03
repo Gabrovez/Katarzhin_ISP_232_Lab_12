539f2fbb63907ed82d3fd931923a1060ae42e64e
Gabrovez <gabrovez321@gmail.com>
2025-11-03 05:04:30 +0300

    Самостоятельные задания

---
+++ Clicker/Form1.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace Clicker
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.buttomReset = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.labelTime = new System.Windows.Forms.Label();
+            this.labelPresses = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // buttomReset
+            // 
+            this.buttomReset.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttomReset.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttomReset.Location = new System.Drawing.Point(314, 191);
+            this.buttomReset.Name = "buttomReset";
+            this.buttomReset.Size = new System.Drawing.Size(83, 52);
+            this.buttomReset.TabIndex = 0;
+            this.buttomReset.Text = "Reset";
+            this.buttomReset.UseVisualStyleBackColor = true;
+            this.buttomReset.Click += new System.EventHandler(this.buttomReset_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Clicker.Properties.Resources.maxresdefault_2647550057;
+            this.pictureBox1.Location = new System.Drawing.Point(52, 169);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(207, 113);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // labelTime
+            // 
+            this.labelTime.AutoSize = true;
+            this.labelTime.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelTime.Location = new System.Drawing.Point(47, 51);
+            this.labelTime.Name = "labelTime";
+            this.labelTime.Size = new System.Drawing.Size(122, 29);
+            this.labelTime.TabIndex = 3;
+            this.labelTime.Text = "Time : 10";
+            // 
+            // labelPresses
+            // 
+            this.labelPresses.AutoSize = true;
+            this.labelPresses.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelPresses.Location = new System.Drawing.Point(47, 104);
+            this.labelPresses.Name = "labelPresses";
+            this.labelPresses.Size = new System.Drawing.Size(112, 29);
+            this.labelPresses.TabIndex = 4;
+            this.labelPresses.Text = "Clicks: 0";
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(464, 319);
+            this.Controls.Add(this.labelPresses);
+            this.Controls.Add(this.labelTime);
+            this.Controls.Add(this.buttomReset);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Clicker";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttomReset;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label labelTime;
+        private System.Windows.Forms.Label labelPresses;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ Clicker/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Clicker
+{
+    public partial class Form1 : Form
+    {
+        private int time_left = 10;
+        private int countclc = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+            timer1.Interval = 1000;
+        }
+        private void buttomReset_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();
+            time_left = 10;
+            labelTime.Text = "Time: " + time_left;
+            countclc = 0;
+            labelPresses.Text = "Clicks: " + countclc;
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+                timer1.Start();
+                countclc++;
+                labelPresses.Text = "Clicks: " + countclc;
+        }
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            time_left--;
+            if (time_left < 0)
+            {
+                timer1.Stop();
+                MessageBox.Show($"Congraulations! In 10 seconds you managed to click {countclc} times! ");
+                time_left = 10;
+                labelTime.Text = "Time: " + time_left;
+                countclc = 0;
+                labelPresses.Text = "Clicks: " + countclc;
+            }
+            else
+            {
+                labelTime.Text = "Time: " + time_left;
+            }
+        }
+    }
+}
